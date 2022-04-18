d9fcd31b0be214de042d5011de05e590ec4a34b9
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2022-04-18 13:16:25 -0400

    Update RPSysSceneCreator.h

--- include/RP/RPSystem/RPSysSceneCreator.h
+++ include/RP/RPSystem/RPSysSceneCreator.h
@@ -143,23 +143,4 @@ private:
     static RPSysSceneCreator *sInstance;
 };
 
-RPSysSceneCreator::SceneAttributes RPSysSceneCreator::sSceneAttributeTable[] = {
-    // Scene                       pack         c    s    e    u       resDir                 name
-    {RP_BOOT_SCENE,             RP_ALLPACK,     0, FALSE, 1, TRUE,  "RPCommon/",             "ロゴ"},
-    {RP_PLAYER_SELECT_SCENE,    RP_ALLPACK,     0, FALSE, 2, TRUE,  "RPCommon/",             "プレイヤーセレクト"},
-    {RP_NUNCHAKU_SCENE,         RP_ALLPACK,     1, FALSE, 3, FALSE, "RPCommon/",             "ヌンチャクチェック"},
-    {RP_BSB_SCENE,              RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPBsbScene/",           "野球"},
-    {RP_BOW_SCENE,              RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPBowScene/",           "ボウリング"},
-    {RP_GOL_SCENE,              RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPGolScene/",           "ゴルフ"},
-    {RP_TNS_SCENE,              RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPTnsScene/",           "テニス"},
-    {RP_BOX_SCENE,              RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPBoxScene/",           "ボクシング"},
-    {RP_SPORTS_TITLE_SCENE,     RP_SPORTS_PACK, 0, FALSE, 1, TRUE,  "RPSportsTitle/",        "スポーツパック"},
-    {RP_SPORTS_MENU_SCENE,      RP_SPORTS_PACK, 0, FALSE, 2, TRUE,  "RPSportsCommon/",       "スポーツメニューセレクト"},
-    {RP_TRAIN_MENU_SCENE,       RP_SPORTS_PACK, 1, FALSE, 3, TRUE,  "RPSportsTrainingMenu/", "トレーニングメニュー"},
-    {RP_PHYSICAL_MENU_SCENE,    RP_SPORTS_PACK, 1, FALSE, 3, TRUE,  "RPSportsPhysical/",     "体力測定メニュー"},
-    {RP_PHYSICAL_PREVIEW_SCENE, RP_SPORTS_PACK, 1, FALSE, 3, TRUE,  "RPSportsPhysical/",     "体力測定説明"},
-    {RP_PHYSICAL_RESULT_SCENE,  RP_SPORTS_PACK, 1, FALSE, 3, TRUE,  "RPSportsPhysical/",     "体力測定結果"},
-    {RP_GOL_SELECT_SCENE,       RP_SPORTS_PACK, 1, FALSE, 0, FALSE, "RPGolScene/",           "ゴルフコースセレクト"},
-};
-
 #endif
